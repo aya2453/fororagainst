015a96885da7b0cbb0ee414a31d881545420e01a
Aya Nakagawae <aya.nk.0116@gmail.comm>
2016-02-12 01:35:47 +0900

    トップページ、投稿ページ、詳細ページ、投稿機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,21 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.permit(:title, :thumbnail, :content)
   end
 end
