1cd0ea320d58ce64943ab7bce99e33ffa0d6e439
Aya Nakagawae <aya.nk.0116@gmail.comm>
2016-02-20 02:36:37 +0900

    投稿時ログインしていなかったらリダイレクト

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @articles = Article.all
@@ -21,7 +21,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.permit(:title, :thumbnail, :content)
     end
-    def move_to_index
-      redirect_to action: :index unless user_signed_in?
-    end
+
 end
